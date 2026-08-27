a8fa6df0d038e348bd081ef889706ad1df47c412
Vampeta <125282998+MiguelSerafim7@users.noreply.github.com>
2026-08-27 01:36:56 +0000

    salvo

---
+++ ecommerce/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into categoria (nome, descricao) values ('Informatica', 'Produtos de Informatica');
+
+insert into categoria (nome, descricao) values ('Livros', 'Livros Tecnicos');
+
+insert into produto (nome, descricao, preco, estoque, categoria_id ) values ('Codigo limpo', 'Livro do Robert C Martin', 87.34, 20, 2);
