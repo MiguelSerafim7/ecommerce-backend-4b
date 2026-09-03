9d5d57540dbd6264af9976b686dbd7fd19d4e6ec
Vampeta <125282998+MiguelSerafim7@users.noreply.github.com>
2026-09-03 01:32:51 +0000

    salvar

--- ecommerce/src/main/resources/import.sql
+++ ecommerce/src/main/resources/import.sql
@@ -3,3 +3,9 @@ insert into categoria (nome, descricao) values ('Informatica', 'Produtos de Info
 insert into categoria (nome, descricao) values ('Livros', 'Livros Tecnicos');
 
 insert into produto (nome, descricao, preco, estoque, categoria_id ) values ('Codigo limpo', 'Livro do Robert C Martin', 87.34, 20, 2);
+
+insert into cliente (nome, email, telefone) values ('miguel', 'Miguel@Gmail.com', '18997965327');
+
+insert into pedido (data, status, valor_total, cliente_id) values ('2026-09-02 20:13:10', 'Pago', '35', '1');
+
+insert into item_pedido (pedido_id, produto_id, quantidade, valor_unitario) values ('1', '2', '5', '100');
